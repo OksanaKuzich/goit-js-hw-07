918fa92beec4bb637af5270a21ec524860a95a58
OksanaKuzich <komoks255@gmail.com>
2022-08-27 23:16:59 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+function createMarkUp(galleryItems) {
+  return galleryItems
+    .map((image) => {
+      return `
+<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`;
+    })
+    .join("");
+}
+
+const createMarkUpGallery = createMarkUp(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.innerHTML = createMarkUpGallery;
+
+galleryEl.addEventListener("click", onElementClick);
+
+function onElementClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+}
+
+new SimpleLightbox(".gallery__item", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
