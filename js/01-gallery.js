4113f1d6abcb8fca6b260bda6251bd08846881b1
OksanaKuzich <komoks255@gmail.com>
2022-08-27 22:30:37 +0300

    task 1 gallery

---
+++ js/01-gallery.js
@@ -0,0 +1,58 @@
+import { galleryItems } from "./gallery-items.js";
+
+// Change code below this line
+
+function createMarkUp(galleryItems) {
+  return galleryItems
+    .map((image) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+const createMarkUpGallery = createMarkUp(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.innerHTML = createMarkUpGallery;
+
+galleryEl.addEventListener("click", onElementClick);
+
+function onElementClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `
+<img src="${event.target.dataset.source}" width="800" height="600" alt="${event.target.alt}">
+`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", closeByEscape);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", closeByEscape);
+      },
+    }
+  );
+
+  instance.show();
+}
+
+function closeByEscape(event) {
+  console.log(event.code);
+  if (event.keyCode === "Escape") {
+    instance.close;
+  }
+}
